32709708a31a21c1a64244cf2237c01992fd8ba5
Sergey Lukichev <sergey.lukichev@zalando.de>
2023-01-13 14:51:35 +0100

    test green

--- sleepin/src/main/java/SleepIn.java
+++ sleepin/src/main/java/SleepIn.java
@@ -1,5 +1,10 @@
 public class SleepIn {
     public static boolean sleepIn(boolean weekday, boolean vacation) {
+
+        if (!weekday || vacation) {
+            return true;
+        }
+
         return false;
     }
 }
